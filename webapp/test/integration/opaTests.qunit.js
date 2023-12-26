6e7aeb698fab6eaf91e159099a987990eb69d5f1
Thomas Mitchell <thomas.mitchell.13@gmail.com>
2023-12-26 20:08:58 +0000

    init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["hipt/hipt/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"hipt/hipt/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
