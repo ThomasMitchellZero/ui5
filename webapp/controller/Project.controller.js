d700b4b7b41a97f150767784cf5d1138b58efe7d
Thomas Mitchell <thomas.mitchell.13@gmail.com>
2023-12-29 19:12:29 +0000

    changed some file names, deleted tsconfig

--- webapp/controller/Project.controller.js
+++ webapp/controller/Project.controller.js
@@ -1,14 +1,20 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
-        return Controller.extend("hipt.hipt.controller.View1", {
+        return Controller.extend("hipt.hipt.controller.Project", {
             onInit: function () {
+
+                const jsonArr = {"phaseArr": ""}
+                const abc = Object.entries(jsonArr)
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, "customer");
 
             }
         });
---
+++ webapp/controller/SubtaskCard.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+
+ ], (Controller, JSONModel) => {
+    "use strict";
+ 
+    return Controller.extend("hipt.hipt.controller.PhaseCard", {
+
+        
+        onInit: function() {
+
+            // OK.  I *THINK* that even if I can just get the keys, I should still be OK?  
+            
+        }
+    });
+ });
