2f3c7891c707ffa0f3f574c04a5608435529e5c1
Thomas Mitchell <thomas.mitchell.13@gmail.com>
2024-01-02 17:04:48 +0000

    added logic to set activeScope based on URL.  Rendering correctly.

--- webapp/controller/StepDetails.controller.js
+++ webapp/controller/StepDetails.controller.js
@@ -3,6 +3,7 @@ sap.ui.define(
         "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel",
     ],
     function (BaseController, JSONModel) {
+        //"What are you doing, Step-Detail?"
         "use strict";
 
         return BaseController.extend("hipt.hipt.controller.StepDetails", {
@@ -10,16 +11,33 @@ sap.ui.define(
             onInit: function () {
 
                 const oRouteArgs = this.getOwnerComponent().navProps
+                const sPhaseKey = oRouteArgs["?phase"]
+                const sTaskKey = oRouteArgs["?task"]
+                const sSubtaskKey = oRouteArgs["?subtask"]
 
-                const mRouteArgs = this.getOwnerComponent().getModel("oRouteParams")
 
-                const oStepData = this.getOwnerComponent().getModel("progressObj").getData()
+                // Possible scopes for StepDetails
+                const oProject = this.getOwnerComponent().getModel("progressObj").getData()
+                const oPhase = oProject?.phases?.[sPhaseKey]
+                const oPhaseTask = oPhase?.tasks?.[sTaskKey]
+                const oSubtasks = oPhaseTask?.subtasks?.[sSubtaskKey]
 
-                const oActiveStep = oStepData.phases;
+                const _setActiveScope = (oScope, sSubstepKey) => {
+                    oScope.aSteps = Object.entries(oScope[sSubstepKey] || {})
+                    return oScope
+                }
 
-                const aStepData = new JSONModel({ stepsArr: Object.entries(oStepData.phases) })
+                const oActiveScope =
+                    oPhaseTask ? _setActiveScope(oPhaseTask, "subtasks") :
+                        oPhase ? _setActiveScope(oPhase, "tasks") :
+                            _setActiveScope(oProject, "phases")
 
-                this.getView().setModel(aStepData, "Step_Data");
+                this.getView().setModel(new JSONModel(oActiveScope), "ActiveScope")
+
+
+
+
+
 
                 const testObj = {
                     arr: [{ n: "a" }, { n: "b" }, { n: "c" }]
