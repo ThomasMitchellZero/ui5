4b8169563fae07c8f33bc9b51fafc5354714a7cc
Thomas Mitchell <thomas.mitchell.13@gmail.com>
2024-01-02 22:42:09 +0000

    Cleaned up the loop to check levels and create substep arr

--- webapp/controller/StepDetails.controller.js
+++ webapp/controller/StepDetails.controller.js
@@ -19,22 +19,81 @@ sap.ui.define(
                 // Possible scopes for StepDetails
                 const oProject = this.getOwnerComponent().getModel("progressObj").getData()
                 const oPhase = oProject?.phases?.[sPhaseKey]
-                const oPhaseTask = oPhase?.tasks?.[sTaskKey]
-                const oSubtasks = oPhaseTask?.subtasks?.[sSubtaskKey]
+                const oTask = oPhase?.tasks?.[sTaskKey]
+                const oSubtask = oTask?.subtasks?.[sSubtaskKey]
 
-                const _setActiveScope = (oScope, sSubstepKey) => {
-                    oScope.aSteps = Object.entries(oScope[sSubstepKey] || {})
-                    return oScope
+                const aScopeRts = [
+                    { rt: oSubtask, step: "subtask", substep: null },
+                    { rt: oTask, step: "task", substep: "subtasks" },
+                    { rt: oPhase, step: "phase", substep: "tasks" },
+                    { rt: oProject, step: "project", substep: "phases" },
+                ]
+
+                let sActiveScopeLevel = ""
+
+
+                /*
+                    const _setActiveScope = (scopeStr, substepKeyStr) => {
+                    const oTargetScope = oScopeLevel[scopeStr]
+                    oTargetScope.aSteps = Object.entries(oTargetScope[substepKeyStr] || {})
+                    sActiveScopeLevel = scopeStr
+                    return oScopeLevel
                 }
+                
+                */
 
-                const oActiveScope =
+
+                /*
+
+                                const oActiveScope =
                     oPhaseTask ? _setActiveScope(oPhaseTask, "subtasks") :
                         oPhase ? _setActiveScope(oPhase, "tasks") :
                             _setActiveScope(oProject, "phases")
 
                 this.getView().setModel(new JSONModel(oActiveScope), "ActiveScope")
 
+                                const oActiveScope =
+                    oScopeLevel.task ? _setActiveScope("task", "subtasks") :
+                        oScopeLevel.phase ? _setActiveScope("phase", "tasks") :
+                            _setActiveScope("project", "phases")
+
                 
+                */
+
+                const _setActiveScope = () => {
+                    let output = {}
+                    for (let item of aScopeRts) {
+                        if (item.rt) {
+                            output = item.rt
+                            output.aSteps = Object.entries(output[item.substep] || {})
+                            sActiveScopeLevel = item.step
+                            break
+                        }
+                    }
+                    return output
+                }
+
+                const oActiveScope = _setActiveScope()
+
+                /*
+                                for(let item of aScopeRts){
+                    if(item.rt){
+                        let oActiveScope = item.rt
+                        oActiveScope.aSteps = Object.entries(oActiveScope[item.substep] || {})
+                        sActiveScopeLevel = item.step
+                        break
+                    }
+                }
+                
+                
+                */
+
+
+
+
+                this.getView().setModel(new JSONModel(oActiveScope), "ActiveScope")
+
+
                 const stopper = "farrt"
 
                 /*
@@ -43,8 +102,17 @@ sap.ui.define(
                 */
             },
 
-            onStepClick: function(){
+            onStepClick: function (oEvent) {
+                const oRouter = this.getOwnerComponent().getRouter();
+                const oItem = oEvent.getSource()
+                const oPars = oItem.getBindingContext("ActiveScope");
+                const oContext = oPars.getObject();
+                const oData = oEvent.getSource().getBindingContext("ActiveScope").getObject()
 
+
+
+
+                const stopper = "farrt"
             }
         });
     }
